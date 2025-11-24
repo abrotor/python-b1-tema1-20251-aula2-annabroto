97cb43b1b21f816fa156a974e5a35133c16341c6
abrotor <abrotor@uoc.edu>
2025-11-24 21:26:15 +0100

    tots

--- 1b/ej1b4.py
+++ 1b/ej1b4.py
@@ -66,8 +66,19 @@ import numpy as np
 
 def results(list_numbers):
     # Write here your code
+    average = np.average(list_numbers)
+    average = round(average, 2)
+    std = np.std(list_numbers)
+    std = round(std, 2)
+
+    print("Average:", average)
+    print("Standard deviation:", std)
+
+    return average, std
+
+
     pass
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
+results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
--- 1c/ej1c4.py
+++ 1c/ej1c4.py
@@ -42,13 +42,20 @@ Exemple:
 
 def is_palindrome(word):
     # Write here your code
+    palindron = True
+    if len(word) <= 1:
+        return palindron
+    else:
+        if word[0] != word[-1]:
+            palindron = False
+        return palindron and is_palindrome(word[1:-1])
     pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# word = "level"
-# print(f"Is '{word}' word palindrome?", is_palindrome(word))
+word = "level"
+print(f"Is '{word}' word palindrome?", is_palindrome(word))
 #
-# word = "juan"
-# print(f"Is '{word}' word palindrome?", is_palindrome(word))
+word = "juan"
+print(f"Is '{word}' word palindrome?", is_palindrome(word))
